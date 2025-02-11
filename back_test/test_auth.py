e08c7a746e2bb0f266c6ecd775ec9f157df4ee92
Chad Estoup--Streiff <chadestoupstreiff@gmail.com>
2025-02-11 13:49:11 +0100

    Fix tests

--- back_test/test_auth.py
+++ back_test/test_auth.py
@@ -8,7 +8,13 @@ url = "http://127.0.0.1:8081"
 email = f"test_{test_uuid}@gmail.com"
 password = f"testpwd_{test_uuid}"
 full_name = "Test User"
+token = None
+card_id = None
 
+
+##############
+# Auth tests #
+##############
 
 def test_url():
     response = requests.get(f"{url}/docs")
@@ -40,6 +46,9 @@ def test_login():
     assert response.status_code == 200
     assert "access_token" in response.json()
 
+    global token
+    token = response.json().get("access_token")
+
 
 def test_cant_login_wrong_password():
     response = requests.post(
@@ -60,13 +69,6 @@ def test_cant_login_wrong_email():
 
 
 def test_token_validity():
-    response = requests.post(f"{url}/auth/login?email={email}&password={password}")
-
-    assert response.status_code == 200
-    assert "access_token" in response.json()
-
-    token = response.json().get("access_token")
-
     response = requests.get(f"{url}/auth/info?token={token}")
 
     assert response.status_code == 200
@@ -75,81 +77,30 @@ def test_token_validity():
     assert "full_name" in response.json()
 
 
-def register_user():
-    response = requests.post(
-        f"{url}/auth/register?email={email}&password={password}&full_name={full_name}"
-    )
+###############
+# Cards tests #
+###############
 
-    assert response.status_code == 200
-    assert response.json().get("message") == "User registered successfully"
-
-
-def login_user():
-    response = requests.post(f"{url}/auth/login?email={email}&password={password}")
-
-    assert response.status_code == 200
-    assert "access_token" in response.json()
-
-    token = response.json().get("access_token")
-
-    return token
-
-
-def test_draw_card(token):
+def test_draw_card():
     response = requests.get(f"{url}/cards/draw?token={token}")
 
     assert response.status_code == 200
     assert "id" in response.json()
 
-    return response.json()
+    global card_id
+    card_id = response.json().get("id")
 
 
-def test_check_card_in_inventory(token, card_id):
+
+def test_check_card_in_inventory():
     response = requests.get(f"{url}/cards/inventory?token={token}")
 
     assert response.status_code == 200
-    assert card_id in response.json()
+    assert any(card['card_id'] == card_id for card in response.json())
 
 
-def test_get_card_info(token, card_id):
+def test_get_card_info():
     response = requests.get(f"{url}/cards/get?card_id={card_id}")
 
     assert response.status_code == 200
     assert "id" in response.json()
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.info("Starting tests...")
-    logging.info(f"Test url: {url}")
-    logging.info(f"Test uuid: {test_uuid}")
-    logging.info(f"Test email: {email}")
-    logging.info(f"Test password: {password}")
-    logging.info(f"Test full name: {full_name}")
-
-    test_url()
-    test_register()
-    test_cant_register_same_email()
-    test_login()
-    test_cant_login_wrong_password()
-    test_cant_login_wrong_email()
-    test_token_validity()
-
-    test_uuid = uuid.uuid4()
-    url = "http://127.0.0.1:8081"
-    email = f"test_{test_uuid}@gmail.com"
-    password = f"testpwd_{test_uuid}"
-    full_name = "Test User"
-    logging.info(f"Test url: {url}")
-    logging.info(f"Test uuid: {test_uuid}")
-    logging.info(f"Test email: {email}")
-    logging.info(f"Test password: {password}")
-    logging.info(f"Test full name: {full_name}")
-
-    register_user()
-    token = login_user()
-    card = test_draw_card(token)
-    test_check_card_in_inventory(token, card.id)
-    test_get_card_info(token, card.id)
-
-    logging.info("Ok!")
